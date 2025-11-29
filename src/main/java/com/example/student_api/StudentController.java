ac30b6dafb9358de45d7706593e88d7fa4e70d16
randomchaffee <107630700+randomchaffee@users.noreply.github.com>
2025-11-29 21:13:53 +0800

    Add CRUD operations to StudentController and StudentService

--- src/main/java/com/example/student_api/StudentController.java
+++ src/main/java/com/example/student_api/StudentController.java
@@ -7,20 +7,40 @@ import java.util.List;
 @RequestMapping("/students")
 public class StudentController {
 
-    private final StudentService service;
+    StudentService service;
 
     public StudentController(StudentService service) {
         this.service = service;
     }
 
+    // get ALL students
     @GetMapping
     public List<Student> getAllStudents() {
         return service.getAll();
     }
-
+    
+    // get student
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable int id) {
+    	return service.getStudentById(id);
+    }
+    
+    // add a student
     @PostMapping
     public String addStudent(@RequestBody Student s) {
         service.addStudent(s);
         return "Student added!";
     }
-}
+    
+    // update a student
+    @PutMapping("/{id}")
+    public String updateStudent(@PathVariable int id, @RequestBody Student student) {
+    	return service.updateStudent(id, student);
+    }
+    
+    // delete a student
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable int id) {
+    	return service.deleteStudent(id);
+    }
+}	
--- src/main/java/com/example/student_api/StudentService.java
+++ src/main/java/com/example/student_api/StudentService.java
@@ -11,8 +11,50 @@ public class StudentService {
     public List<Student> getAll() {
         return students;
     }
-
+    
+    
+    
+    
+    
+    
+    // Methods
+    
+    // find a student in the list using its id
+    public Student getStudentById(int id) {
+    	return students.stream()
+    			.filter(s -> s.getId() == id)
+    			.findFirst()
+    			.orElse(null);
+    }
+    
+    // add a student to data list
     public void addStudent(Student s) {
         students.add(s);
     }
+    
+    // update student values
+    public String updateStudent(int id, Student updatedStudent) {
+    	Student existing = getStudentById(id);
+    	if (existing == null) {
+    		return "Student not found.";
+    	}
+    	
+    	existing.setName(updatedStudent.getName());
+    	existing.setAge(updatedStudent.getAge());
+    	existing.setCourse(updatedStudent.getCourse());
+    	
+    	return "Student updated Succesfully.";
+    }
+    
+    // delete a student
+    public String deleteStudent(int id) {
+    	Student existing = getStudentById(id);
+    	if (existing == null) {
+    		return "Student not found.";
+    	}
+    	
+    	students.remove(existing);
+    	return "Student deleted successfully.";
+    }
+    
 }
