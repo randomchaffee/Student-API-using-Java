e97038689e0ae02ed9fb1b0594eabbd9903b5e81
randomchaffee <107630700+randomchaffee@users.noreply.github.com>
2025-12-03 21:47:59 +0800

    Fix unnecessary curly brace

--- src/main/java/com/example/student_api/Student.java
+++ src/main/java/com/example/student_api/Student.java
@@ -42,6 +42,5 @@ public class Student {
     public void setAge(Integer age) { this.age = age; }
     public void setCourse(String course) { this.course = course; }
     public void setId(Long id) { this.id = id; }
-    }
     
 }
