b9c9aed3ad5f17e9b2da2ef3a5511792170077fb
randomchaffee <107630700+randomchaffee@users.noreply.github.com>
2025-12-15 19:21:52 +0800

    Remove unused import in GlobalExceptionHandler

--- src/main/java/com/randomchaffee/student_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/randomchaffee/student_api/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@
 package com.randomchaffee.student_api.exception;
 
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
