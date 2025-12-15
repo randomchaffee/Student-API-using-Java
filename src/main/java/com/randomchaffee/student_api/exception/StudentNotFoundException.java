a079793ff4f1db091cca1e79a1b2662fec6b10b4
randomchaffee <107630700+randomchaffee@users.noreply.github.com>
2025-12-15 19:22:08 +0800

    Add serialVersionUID to StudentNotFoundException

--- src/main/java/com/randomchaffee/student_api/exception/StudentNotFoundException.java
+++ src/main/java/com/randomchaffee/student_api/exception/StudentNotFoundException.java
@@ -1,6 +1,11 @@
 package com.randomchaffee.student_api.exception;
 
 public class StudentNotFoundException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public StudentNotFoundException(Long id) {
 		super("Student with id " + id + " not found.");
 	}
